409c63160664862fbc2553f77a3e3a028b02c692
AndreyDodonov <Nordhummer@gmail.com>
2020-02-19 01:18:47 +0300

    [dev]after install carousel

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,19 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import img1 from '../images/img1.jpg';
 import img2 from '../images/img2.jpg';
 import img3 from '../images/img3.jpg';
 import arrow from '../images/svg/arrow.svg';
 import round from '../images/svg/round.svg';
+import {Carousel} from "react-bootstrap";
 
 const Slider = props => {
+
+    function ControlledCarousel() {
+        const [index, setIndex] = useState(0);
+
+        const handleSelect = (selectedIndex, e) => {
+            setIndex(selectedIndex);
+        };
+
+        return (
+            <Carousel activeIndex={index} onSelect={handleSelect} >
+                <Carousel.Item>
+                    <img
+                        className="d-block w-30"
+                        src={img1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-30"
+                        src={img2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-30"
+                        src={img3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        );
+    }
+
+
     return (
 
             <div className="slider">
                 <h1>{props.galleryTitle}</h1>
                 <div className="slider__gallery">
-                    <img className="slider__gallery-image" src={img1} alt="image1"/>
-                    <img className="slider__gallery-image" src={img2} alt="image1"/>
-                    <img className="slider__gallery-image" src={img3} alt="image1"/>
+                    <ControlledCarousel />
                 </div>
                 <div className="slider__buttons">
                     <img src={arrow} alt="arrow left"/>
